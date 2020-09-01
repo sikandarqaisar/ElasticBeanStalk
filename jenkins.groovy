0c004de9256abe6ae22cf6b05641465b5e11264b
Sikandar <sikandar@eurustechnologies.com>
2020-09-01 16:55:54 +0500

    dev branch push

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,95 +1,11 @@
 pipeline {
-agent any
-stages{
-    stage('CheckoutModule1') {
-        steps {
-            dir("Module1")
-            {
-                git branch: "master",
-                credentialsId: '6c0f262c-df9d-4146-a869-573d48901536',
-                url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git'
-            }
-        }
-    }
-
-    stage('CheckoutModule2') {
-        steps {
-            dir("Module2")
-            {
-                git branch: "master",
-                credentialsId: '6c0f262c-df9d-4146-a869-573d48901536',
-                url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git'
-            }
-            sh 'ls'
-            sh 'cd ..'
-            sh 'cd Module1'
-            sh 'ls'
-            sh 'cd ..'
-            sh 'cd ..'
-            sh 'ls'
-        }
-    }
-  }
-}
-
-
-
-pipeline {
-agent any
-stages{
-    stage('Clone another repository to subdir') {
-      steps {
-        sh 'ls'
-        sh 'mkdir subdir1'
-        dir ('subdir1') {
-            git branch: 'master',
-            credentialsId: '4f722b6c-18b6-45eb-9c12-c60dea27578f',
-            url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git'
-        }
-      }
-    }
-    stage('Clone another repository to masterDir') {
-      steps {
-        sh 'ls' 
-        sh 'mkdir masterDir'
-        dir ('masterDir') {
-            git branch: 'master',
-            credentialsId: '4f722b6c-18b6-45eb-9c12-c60dea27578f',
-            url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git'
-        }
-      }
-    }
-  }
-}
-
- 
-
-
-pipeline {
     agent any
+    parameters {
+    string(defaultValue: "master", name: 'BranchName1stRepo')
+    string(defaultValue: "master", name: 'BranchName2ndRepo')
+    }    
     stages {
-        stage('1st') {
-            steps {
-                git url:'https://github.com/sikandarqaisar/ElasticBeanStalk.git'
-                sh 'git rev-parse --abbrev-ref HEAD'
-            }
-        }
-    //     stage('2nd') {
-    //         steps {
-    //             checkout([$class: 'GitSCM',
-    //                 branches: [[name: 'any']],
-    //                 userRemoteConfigs: [[url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git']]
-    //             ])
-    //         }
-    //     }
-    }
-}
-
-
-pipeline {
-    agent any
-    stages {
-        stage('Checkout') {
+        stage('Checkout 1st Repo') {
             steps{
                 checkout([  
                             $class: 'GitSCM', 
@@ -99,21 +15,35 @@ pipeline {
                             submoduleCfg: [], 
                             userRemoteConfigs: [[url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git']]
                         ])
-                sh 'git rev-parse --abbrev-ref HEAD'
-                checkout([  
-                            $class: 'GitSCM', 
-                            branches: [[name: 'refs/heads/master']], 
-                            doGenerateSubmoduleConfigurations: false, 
-                            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'Combination']], 
-                            submoduleCfg: [], 
-                            userRemoteConfigs: [[url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git']]
-                        ])                
-                sh 'GitSCM.branches[0].name'
+                dir("CalibrationResults"){
+                    script {
+                        sh 'git name-rev --name-only HEAD'
+                    }
+                }
+                
+                script {
+                        echo "Triggering 1st job"
+                        FULL_PATH_BRANCH = 'git name-rev --name-only HEAD'
+                        BranchName1stRepo = FULL_PATH_BRANCH.substring(FULL_PATH_BRANCH.lastIndexOf('/') + 1, FULL_PATH_BRANCH.length())
+                        echo BranchName1stRepo
+                        build job: "1stRepo", wait: false, parameters: [string(name: 'branch_name', value: String.valueOf(BranchName1stRepo))]
+                }                        
             }
         }
-        stage('2nd') {
+        stage('Checkout 2nd Repo') {
             steps {
-                sh "ls"
+                checkout([  
+                            $class: 'GitSCM', 
+                            branches: [[name: '']], 
+                            doGenerateSubmoduleConfigurations: false, 
+                            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CalibrationResults']], 
+                            submoduleCfg: [], 
+                            userRemoteConfigs: [[url: 'https://github.com/sikandarqaisar/ElasticBeanStalk.git']]
+                        ])
+                script {
+                        echo "Triggering 2nd job"
+                        build job: "2ndRepo", wait: false, parameters: [string(name: 'branch_name', value: String.valueOf(BranchName2ndRepo))]
+                }
             }
         }
     }
